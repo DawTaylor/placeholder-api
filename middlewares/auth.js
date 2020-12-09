04c902fab9893c3f94ef222f51580b478b29066c
Adalberto Taylor <adalbertotaylor84@gmail.com>
2020-12-09 17:00:47 -0300

    Linting files

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,17 @@
-const VALID_AUTH = 'VGVzdGU6bG85OzhxNjdld2ZyZ3RydGY='
+const VALID_AUTH = 'VGVzdGU6bG85OzhxNjdld2ZyZ3RydGY=';
 
 const authMiddleware = (req, res, next) => {
-  const { authorization } = req.headers
+  const { authorization } = req.headers;
 
-  if(!authorization) {
-    return res.status(401).end()
+  if (!authorization) {
+    return res.status(401).end();
   }
 
-  if(authorization.split(' ')[1] !== VALID_AUTH) {
-    return res.status(403).end()
+  if (authorization.split(' ')[1] !== VALID_AUTH) {
+    return res.status(403).end();
   }
 
-  return next()
-}
+  return next();
+};
 
-module.exports = { authMiddleware, VALID_AUTH }
+module.exports = { authMiddleware, VALID_AUTH };
--- routes/product.js
+++ routes/product.js
@@ -1,19 +1,20 @@
-const Express = require( 'express');
+const Express = require('express');
+
 const { Router } = Express;
 
-const mocks = require('../mocks/products')
+const mocks = require('../mocks/products');
 
 const productRouter = new Router();
 
-const flatProducts = Object.keys(mocks).reduce(( acc, key ) => ([ ...acc, ...mocks[key]]), [])
+const flatProducts = Object.keys(mocks).reduce((acc, key) => ([...acc, ...mocks[key]]), []);
 
 productRouter.get('/product/:productId', (req, res) => {
-  const { productId } = req.params
+  const { productId } = req.params;
 
-  const product = flatProducts.find(product => product.sku === productId)
+  const product = flatProducts.find((p) => p.sku === productId);
 
-  if(!product) { 
-    return res.status(404).end()
+  if (!product) {
+    return res.status(404).end();
   }
 
   return res.json(product);
