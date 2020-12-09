d89e8ddc5bf4c3147725ee1338d14d99733acef8
Adalberto Taylor <adalbertotaylor84@gmail.com>
2020-12-09 14:31:13 -0300

    Adds similar and product routes

---
+++ routes/product.js
@@ -0,0 +1,22 @@
+const Express = require( 'express');
+const { Router } = Express;
+
+const mocks = require('../mocks/products')
+
+const productRouter = new Router();
+
+const flatProducts = Object.keys(mocks).reduce(( acc, key ) => ([ ...acc, ...mocks[key]]), [])
+
+productRouter.get('/product/:productId', (req, res) => {
+  const { productId } = req.params
+
+  const product = flatProducts.find(product => product.sku === productId)
+
+  if(!product) { 
+    return res.status(404).end()
+  }
+
+  return res.json(product);
+});
+
+module.exports = { productRouter };
---
+++ routes/similar.js
@@ -0,0 +1,27 @@
+const Express = require('express');
+const { Router } = Express;
+
+const mocks = require('../mocks/products.js');
+const validProducts = require('../mocks/validProducts');
+
+const similarRouter = new Router();
+
+similarRouter.get('/similar/:productId', (req, res) => {
+  const { productId } = req.params
+  const { limit } = req.query
+
+  if(!validProducts.includes(productId)) {
+    return res.status(404).end()
+  }
+  
+  const products = mocks[productId] || []
+
+  if(limit && limit < products.length) {
+    const limitedQuantityProducts = products.slice(limit)
+    return res.json({ similar: limitedQuantityProducts })
+  }
+
+  return res.json({ similar: products })
+});
+
+module.exports = { similarRouter };
