efed2c61b45f07d3de4a737420af0ac559ca0369
Adalberto Taylor <adalbertotaylor84@gmail.com>
2021-02-10 15:51:34 -0300

    Fix tests

--- tests/similar.test.js
+++ tests/similar.test.js
@@ -1,9 +1,6 @@
 const request = require('supertest');
 const app = require('../app');
 const { VALID_AUTH } = require('../middlewares/auth');
-const validProducts = require('../mocks/validProducts');
-
-const VALID_PRODUCT_ID = validProducts[1];
 
 const getRequestPath = (path, limit) => {
   if (!limit) return path;
@@ -11,71 +8,72 @@ const getRequestPath = (path, limit) => {
   return `${path}?limit=${limit}`;
 };
 
-const getRequest = (productId, auth, limit) => {
+const getRequest = (data, auth, limit) => {
+  const productId = '123123123';
   if (!auth) {
-    return request(app).get(`/similar/${productId}`);
+    return request(app).post(`/similar/${productId}`, data);
   }
 
-  return request(app).get(getRequestPath(`/similar/${productId}`, limit)).set({ authorization: `Basic ${auth}` });
+  return request(app).post(getRequestPath(`/similar/${productId}`, limit)).send(data).set({ authorization: `Basic ${auth}` });
 };
 
 describe('GET:/similar/:productId', () => {
   test('Should return 401 if no authorization header is provided', async () => {
-    const response = await getRequest(VALID_PRODUCT_ID);
+    const response = await getRequest({});
 
     expect(response.statusCode).toBe(401);
   });
 
   test('Should return 403 if provided authorization header is invalid', async () => {
-    const response = await getRequest(VALID_PRODUCT_ID, 'invalidAUTH');
+    const response = await getRequest({}, 'invalidAUTH');
 
     expect(response.statusCode).toBe(403);
   });
 
   test('Should return 200 if provided authorization header is valid', async () => {
-    const response = await getRequest(VALID_PRODUCT_ID, VALID_AUTH);
+    const response = await getRequest({ price: 71.99 }, VALID_AUTH);
 
     expect(response.statusCode).toBe(200);
   });
 
-  test('Should return 404 for invalid productId', async () => {
-    const response = await getRequest('invalid', VALID_AUTH);
+  test('Should return 404 for price ', async () => {
+    const response = await getRequest({}, VALID_AUTH);
 
     expect(response.statusCode).toBe(404);
   });
 
-  test(`Should return 1 similar product for productId: ${VALID_PRODUCT_ID}`, async () => {
-    const response = await getRequest(VALID_PRODUCT_ID, VALID_AUTH);
+  test('Should return 1 similar product for price 71.99 ', async () => {
+    const response = await getRequest({ price: 71.99 }, VALID_AUTH);
 
     expect(response.statusCode).toBe(200);
     expect(response.body.similar.length).toBe(1);
   });
 
-  test(`Should return 2 similar products for productId: ${validProducts[0]}`, async () => {
-    const response = await getRequest(validProducts[0], VALID_AUTH);
+  test('Should return 3 similar products for price 99.99', async () => {
+    const response = await getRequest({ price: 99.99 }, VALID_AUTH);
 
     expect(response.statusCode).toBe(200);
-    expect(response.body.similar.length).toBe(2);
+    expect(response.body.similar.length).toBe(3);
   });
 
-  test(`Should return 0 similar products for productId: ${validProducts[2]}`, async () => {
-    const response = await getRequest(validProducts[2], VALID_AUTH);
+  test('Should return 0 similar products for and price 9.99', async () => {
+    const response = await getRequest({ price: 9.99 }, VALID_AUTH);
 
     expect(response.statusCode).toBe(200);
     expect(response.body.similar.length).toBe(0);
   });
 
-  test(`Should return 1 similar product for productId: ${validProducts[0]} and limit: 1`, async () => {
-    const response = await getRequest(validProducts[0], VALID_AUTH, 1);
+  test('Should return 1 similar product for price 99.99 and limit: 1', async () => {
+    const response = await getRequest({ price: 99.99 }, VALID_AUTH, 1);
 
     expect(response.statusCode).toBe(200);
     expect(response.body.similar.length).toBe(1);
   });
 
-  test(`Should return 2 similar products for productId: ${validProducts[0]} and limit: 10`, async () => {
-    const response = await getRequest(validProducts[0], VALID_AUTH, 10);
+  test('Should return 3 similar products for price 99.99 and limit: 10', async () => {
+    const response = await getRequest({ price: 99.99 }, VALID_AUTH, 10);
 
     expect(response.statusCode).toBe(200);
-    expect(response.body.similar.length).toBe(2);
+    expect(response.body.similar.length).toBe(3);
   });
 });
