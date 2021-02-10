4ec7381e44d08e67509572d2b97401427e71e7ed
Adalberto Taylor <adalbertotaylor84@gmail.com>
2021-02-10 14:57:07 -0300

    Removes deprecated route and adds body parser middleware

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const Express = require('express');
+const bodyParser = require('body-parser');
 
 const { similarRouter } = require('./routes/similar');
-const { productRouter } = require('./routes/product');
 const { authMiddleware } = require('./middlewares/auth');
 
 const app = new Express();
+
+app.use(bodyParser.json());
 
 app.get('/', (_, res) => res.json({ status: 'ok' }));
 
 app.use(authMiddleware);
 app.use(similarRouter);
-app.use(productRouter);
 
 module.exports = app;
